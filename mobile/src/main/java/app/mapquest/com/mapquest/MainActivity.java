3d75e294d1b3e66a07949b86e9e527686c1848fe
daniellag <daniellag@google.com>
2015-06-12 18:33:29 +0300

    Added local (not server) notification

--- mobile/src/main/java/app/mapquest/com/mapquest/MainActivity.java
+++ mobile/src/main/java/app/mapquest/com/mapquest/MainActivity.java
@@ -1,8 +1,12 @@
 package app.mapquest.com.mapquest;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,7 +14,6 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.parse.ParseException;
-import com.parse.ParsePush;
 
 import app.mapquest.com.mapquest.api.Getting;
 import app.mapquest.com.mapquest.data.Game;
@@ -75,13 +78,6 @@ public class MainActivity extends Activity {
         printGameInfo();
     }
 
-    public void createPushNotification(View view) {
-        ParsePush push = new ParsePush();
-        push.setChannel("");
-        push.setMessage("The Giants just scored! It's now 2-2 against the Mets.");
-        push.sendInBackground();
-    }
-
 
     /**
      * TESTS AND USAGE ****
@@ -93,6 +89,38 @@ public class MainActivity extends Activity {
     }
 
 
+                /* ---------- ANDROID WEAR ------------- */
+    /**
+     * Handles the button to launch a notification.
+     */
+    public void showNotification(View view) {
+        Notification notification = new NotificationCompat.Builder(MainActivity.this)//.setSmallIcon(R.drawable.shopli)
+                .setContentTitle("MAPCHA")
+                .setContentText("You have just chased it !! Now solve the quiz :) ")
+                .setSmallIcon(R.drawable.ic_launcher) // addAction
+                .build();
+
+        NotificationManager notificationManger =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManger.notify(0, notification);
+    }
+
+
+    /* -- Deprecated server notification -- */
+//    public void createPushNotification(View view) {
+//        Log.i(TAG, "Inside get push");
+//        ParsePush push = new ParsePush();
+//        push.setChannel("");
+//        push.setMessage("You have just chased it !! Now solve the quiz :) ");
+//        try {
+//            Log.i(TAG, "SENT THE MESSAGE");
+//            push.send(); // TODO: Send immediately for demo!
+//        } catch (ParseException e) {
+//            Log.e(TAG, "COULDN'T SEND THE MESSAGE!");
+//            e.printStackTrace();
+//        }
+//    }
 
 
 }
