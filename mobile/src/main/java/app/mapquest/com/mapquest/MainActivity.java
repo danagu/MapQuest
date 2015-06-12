53894e1a56c1b536415ba258b8b5dc0072cdf0b7
daniellag <daniellag@google.com>
2015-06-13 00:00:34 +0300

    Removed the notification from main activity

--- mobile/src/main/java/app/mapquest/com/mapquest/MainActivity.java
+++ mobile/src/main/java/app/mapquest/com/mapquest/MainActivity.java
@@ -1,12 +1,8 @@
 package app.mapquest.com.mapquest;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -91,42 +87,4 @@ public class MainActivity extends Activity {
     public void getByIdTest(View view) throws ParseException {
         Toast.makeText(MainActivity.this, "Got location: " + Getting.getLocationInfoByID("Lx5ruREoOn").toString() , Toast.LENGTH_LONG).show();
     }
-
-
-                /* ---------- ANDROID WEAR ------------- */
-    /**
-     * Handles the button to launch a notification.
-     */
-    public void showNotification(View view) {
-        Notification notification = new NotificationCompat.Builder(MainActivity.this)//.setSmallIcon(R.drawable.shopli)
-                .setContentTitle("MAPCHA")
-                .setContentText("You have just chased it !! Now solve the quiz :) ")
-                .setSmallIcon(R.drawable.ic_launcher) // addAction
-                .build();
-
-        NotificationManager notificationManger =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        notificationManger.notify(0, notification);
-
-        // User pushes on notification
-    }
-
-
-    /* -- Deprecated server notification -- */
-//    public void createPushNotification(View view) {
-//        Log.i(TAG, "Inside get push");
-//        ParsePush push = new ParsePush();
-//        push.setChannel("");
-//        push.setMessage("You have just chased it !! Now solve the quiz :) ");
-//        try {
-//            Log.i(TAG, "SENT THE MESSAGE");
-//            push.send(); // TODO: Send immediately for demo!
-//        } catch (ParseException e) {
-//            Log.e(TAG, "COULDN'T SEND THE MESSAGE!");
-//            e.printStackTrace();
-//        }
-//    }
-
-
 }
